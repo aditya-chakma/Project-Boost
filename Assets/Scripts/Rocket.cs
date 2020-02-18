40f4f1c8d2fba9356e8925ce05a9e07775a1d617
Aitto_Sang <adityachakma199@gmail.com>
2020-02-18 11:04:42 +0600

    Audio added. Movement bug fixed. Vel Drag added.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
+    private Rigidbody m_rigidBody;
+    private AudioSource m_audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = this.GetComponent<Rigidbody>();
+        m_rigidBody = this.GetComponent<Rigidbody>();
+        m_audioSource = GetComponent<AudioSource>();
         Application.targetFrameRate = 60;
     }
 
@@ -16,6 +18,7 @@ public class Rocket : MonoBehaviour
     void Update()
     {
         ProcessInput();
+        Audio();
     }
 
     /**
@@ -24,16 +27,44 @@ public class Rocket : MonoBehaviour
     */
     private void ProcessInput()
     {
-        if( Input.GetKey(KeyCode.Space))
+        
+        //Handle the movement
+        if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up*100000);
+            //rigidBody.AddRelativeForce(Vector3.up*100000);
+            m_rigidBody.AddRelativeForce(Vector3.up);
         }
-        if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime*90);
-        }else if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.forward);
+            m_rigidBody.freezeRotation = true;  //Take manual control of the rotation
+
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime*90);
+            transform.Rotate(-Vector3.forward);
+            m_rigidBody.freezeRotation = true;  //Take manual control of the rotation
+
+        }
+
+        m_rigidBody.freezeRotation = false; //Let  physics control the rotation.
+
+    }
+
+    private void Audio()
+    {
+        /**
+                Process sounds
+                */
+        //Start Playing the audio clip
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_audioSource.Play();
+        }
+        //Stops playing the sound
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            m_audioSource.Stop();
         }
     }
 }
