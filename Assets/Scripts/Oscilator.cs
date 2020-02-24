1848e0d8ba3736226fc9d6dceda6c9b8f18fc4a0
Aditya <adityachakma199@gmail.com>
2020-02-24 11:28:08 +0600

    New Oscilator Script

---
+++ Assets/Scripts/Oscilator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscilator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Range(0,1)]
+    [SerializeField]
+    private float movementFactor;
+    [SerializeField]
+    private float m_Period;
+    [SerializeField]
+    Transform m_TargetPosition;
+    private Vector3 m_startingPosition,moveDirection;
+    bool m_forward;
+    private float m_signFact;
+
+    void Start()
+    {
+
+        m_signFact =0.0f;
+        m_forward = true;
+        m_startingPosition = this.gameObject.transform.position;
+        moveDirection = m_TargetPosition.position - m_startingPosition;
+        //this.gameObject.transform.position = m_startingPosition + moveDirection*movementFactor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = m_startingPosition + moveDirection*movementFactor*Mathf.Sin(m_signFact);
+        if(m_Period!=0)
+        {
+            m_signFact+=2*Mathf.PI*Time.deltaTime/m_Period;
+        }
+        
+    }
+
+    private bool Toogle(bool p)
+    {
+        if(p) return false;
+        else return true;
+    }
+}
